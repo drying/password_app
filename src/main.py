28f71e8d45aa9e5e7229190c4fe87cda9cbe2738
yukimatsu3 <y-matsumoto@oscnet.co.jp>
2026-03-26 16:54:30 +0900

    文字数スライダー追加

--- src/main.py
+++ src/main.py
@@ -13,13 +13,13 @@ def main(page: ft.Page):
     password_field = ft.TextField(label="Password", text_size=20, read_only=True, expand=True)
     
     # パスワード生成ロジック
-    def generate_password():
+    def generate_password(password_length):
         alphabet = string.ascii_letters + string.digits + string.punctuation
-        return ''.join(secrets.choice(alphabet) for _ in range(12))
+        return ''.join(secrets.choice(alphabet) for _ in range(password_length))
 
     # 生成ボタンを押したときの処理
     def on_gererate_click(e):
-        password_field.value = generate_password()
+        password_field.value = generate_password(password_length)
         page.update()
     
     # コピーボタンを押したときの処理
@@ -30,7 +30,24 @@ def main(page: ft.Page):
             await ft.Clipboard().set(password_field.value)
             page.show_dialog(ft.SnackBar("パスワードがコピーされました！"))
 
+    # パスワード文字数スライダー
+    password_length = 12
+
+    def password_number_changed(e):
+        nonlocal password_length
+        password_length = int(e.control.value)
+        lenght_label.value = f"長さ: {password_length}"
+        page.update()
+
+    lenght_label = ft.Text(f"長さ: {password_length}")
+    slider = ft.Slider(min=8, max=20, divisions=12, value=password_length, on_change=password_number_changed)
+
     page.add(
+        ft.Column(
+            controls=[
+                ft.Row(controls=[lenght_label, slider]),
+            ],
+        ),
         ft.Row(
             controls=[
                 password_field,
