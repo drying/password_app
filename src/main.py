1f0296f31424fc126e52be65785a2bd5ecc4ba54
yukimatsu <drying3@gmail.com>
2026-03-26 22:05:38 +0900

    初回コミット

--- src/main.py
+++ src/main.py
@@ -1,68 +1,86 @@
 import flet as ft
-import secrets
-import string
+from password import generate_password
 
 def main(page: ft.Page):
     # ページの設定
     page.title = "パスワード生成・管理アプリ"
-    page.theme_mode = "light"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.window.width = 800
     page.window.height = 400
 
-    # パスワード表示フィールド
-    password_field = ft.TextField(label="Password", text_size=20, read_only=True, expand=True)
-    
-    # パスワード生成ロジック
-    # 文字制御チェックボックス
-    common_symbols = "!@#$%^&*()_+-=[]{}"
-
-    uppercase_checkbox = ft.Checkbox(label="大文字を含む", value=True)
-    lowercase_checkbox = ft.Checkbox(label="小文字を含む", value=True)
-    digit_checkbox = ft.Checkbox(label="数字を含む", value=True) 
-    symbols_checkbox = ft.Checkbox(label="記号を含む", value=True)
-
-    def generate_password(password_length):
-        alphabet = ""
-        if uppercase_checkbox.value:
-            alphabet += string.ascii_uppercase
-        if lowercase_checkbox.value:
-            alphabet += string.ascii_lowercase
-        if digit_checkbox.value:
-            alphabet += string.digits
-        if symbols_checkbox.value:
-            alphabet += common_symbols
-
-        return ''.join(secrets.choice(alphabet) for _ in range(password_length))
-
+    # 関数
     # 生成ボタンを押したときの処理
-    def on_gererate_click(e):
-        password_field.value = generate_password(password_length)
+    def on_generate_click(e):
+        try:
+            password_field.value = generate_password(
+                int(slider.value),
+                uppercase_checkbox.value,
+                lowercase_checkbox.value,
+                digit_checkbox.value,
+                symbols_checkbox.value
+            )
+        except ValueError as ex:
+            page.show_dialog(ft.SnackBar(ft.Text(str(ex))))
         page.update()
     
     # コピーボタンを押したときの処理
     async def copy_button_click(e):
         if not password_field.value:
-            page.show_dialog(ft.SnackBar("パスワードがまだ生成されていません！"))
+            page.show_dialog(ft.SnackBar(ft.Text("パスワードがまだ生成されていません！")))
         else:
             await ft.Clipboard().set(password_field.value)
-            page.show_dialog(ft.SnackBar("パスワードがコピーされました！"))
-
-    # パスワード文字数スライダー
-    password_length = 12
-
-    def password_number_changed(e):
-        nonlocal password_length
-        password_length = int(e.control.value)
-        lenght_label.value = f"長さ: {password_length}"
+            page.show_dialog(ft.SnackBar(ft.Text("パスワードがコピーされました！")))
         page.update()
 
-    lenght_label = ft.Text(f"長さ: {password_length}")
-    slider = ft.Slider(min=8, max=20, divisions=12, value=password_length, on_change=password_number_changed)
+    # テキストフィールドのクリア
+    def on_clear_click(e):
+        if not password_field.value:
+            page.show_dialog(ft.SnackBar(ft.Text("パスワードはまだ生成されていません！")))
+        else:
+            password_field.value = ""
+        page.update()
 
+    # パスワード文字数スライダー
+    def password_number_changed(e):
+        length_label.value = f"長さ: {int(slider.value)}"
+        page.update()
+    
+    # UIパーツ
+    # パスワード表示フィールド
+    password_field = ft.TextField(label="Password", text_size=20, read_only=True, expand=True)
+    
+    # 文字制御チェックボックス
+    uppercase_checkbox = ft.Checkbox(label="大文字を含む", value=True)
+    lowercase_checkbox = ft.Checkbox(label="小文字を含む", value=True)
+    digit_checkbox = ft.Checkbox(label="数字を含む", value=True) 
+    symbols_checkbox = ft.Checkbox(label="記号を含む", value=True)
+
+    length_label = ft.Text(f"長さ: 12")
+    slider = ft.Slider(min=8, max=20, divisions=12, value=12, on_change=password_number_changed)
+
+    generate_button = ft.Button(content="生成", width=120, on_click=on_generate_click)
+    copy_button = ft.Button(content="コピー", width=120, on_click=copy_button_click)    
+    clear_button = ft.Button(content="クリア", width=120, on_click=on_clear_click)
+
+    # 初期生成パスワード
+    password_field.value = generate_password(
+        int(slider.value),
+        uppercase_checkbox.value,
+        lowercase_checkbox.value,
+        digit_checkbox.value,
+        symbols_checkbox.value
+    )
+    
     page.add(
         ft.Column(
             controls=[
-                ft.Row(controls=[lenght_label, slider]),
+                ft.Row(controls=[
+                    length_label, 
+                    slider
+                    ],
+                alignment=ft.MainAxisAlignment.CENTER
+                )
             ],
         ),
         ft.Column(
@@ -72,15 +90,24 @@ def main(page: ft.Page):
                     lowercase_checkbox,
                     digit_checkbox,
                     symbols_checkbox,
-                ])
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+                )
             ],
         ),
         ft.Row(
             controls=[
                 password_field,
-                ft.Button(content="生成", width=120, on_click=on_gererate_click),
-                ft.Button(content="コピー", width=120, on_click=copy_button_click),
-            ]
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        ),
+        ft.Row(
+            controls=[
+                generate_button,
+                copy_button,
+                clear_button
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
         )
     )
 
